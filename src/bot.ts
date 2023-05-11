4aabc04bf746aa60771c82a8170a5d2d49759197
astmating <131445482+astmating@users.noreply.github.com>
2023-05-11 09:27:15 +0800

    bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -72,13 +72,12 @@ export class Bot {
                 content: content
             });
         } else if (rawText.startsWith('/imagine ')) {
-             console.log('errorMsg111');
              const prompt = rawText.substring(9);
-             console.log(prompt);
              errorMsg = await submitTask({
                 state: topic + ':' + talkerName,
                 action: "IMAGINE",
                 prompt: prompt 
+                notifyHook:"http://43.156.94.14:4120/nodify"
             });
             
         }
